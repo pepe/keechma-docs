de9a3c307871495f9445c2ac8c21d262201620c9
Mihael Konjevic <konjevic@gmail.com>
2016-02-13 00:45:55 +0100

    Basic styling for annotated code

--- src/marginalia.js
+++ src/marginalia.js
@@ -1,3 +1,19 @@
+var cheerio = require('cheerio');
+var hljs = require('highlight.js');
+
 module.exports = function(content){
-  return content;
+  var $ = cheerio.load(content);
+  $('td.codes').each(function(i, node){
+    var text = $(node).text();
+    if(text){
+      $(node).html('<pre><code class="language-clojure hljs">' + hljs.highlightAuto(text).value + '</code></pre>');
+    }
+
+  });
+
+  $('script, .footer, style').remove();
+  $('p').closest('tr').addClass('with-content');
+  $('head').append('<link rel="stylesheet" href="../css/site.css"><link rel="stylesheet" href="../css/modifications.css">');
+  
+  return $.html();
 }
